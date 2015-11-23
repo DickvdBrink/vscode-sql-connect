c19d7b7b8de942ca5de1c84e69d9355ede17e072
Dick van den Brink <d_vandenbrink@outlook.com>
2015-11-24 00:25:29 +0100

    Allow connection to a sql server machine and execute statements

--- src/extension.ts
+++ src/extension.ts
@@ -13,12 +13,67 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+	var disposable = vscode.commands.registerCommand('extension.sqlConnect', () => {
 		// The code you place here will be executed every time your command is executed
+		var mssql = require('mssql');
+		var connection = null;
+		var config: any = {};
+		vscode.window.showInputBox({
+			placeHolder: "hostname\\instance",
+			prompt: "Enter hostname and optional instance name"
+		}).then((host) => {
+			config.server = host;
+			return vscode.window.showInputBox({
+				placeHolder: "Username",
+				prompt: "Enter username"
+			});
+		}).then((user) => {
+			config.user = user;
+			return vscode.window.showInputBox({
+				password: true,
+				placeHolder: "Password",
+				prompt: "Enter password"
+			});
+		}).then((password) => {
+			config.password = password;
+			connection = new mssql.Connection(config, function(err) {
+				console.log("ERROR: " + err);
+			});
+		}).then(() => {
+			var output = vscode.window.createOutputChannel("sql");
+			output.show(vscode.ViewColumn.Two);
+			output.appendLine("Connecting to server");
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+			loop();
+
+			function loop() {
+				return vscode.window.showInputBox({
+					placeHolder: "SQL"
+				}).then((sql) => {
+					if (sql !== undefined) {
+						output.appendLine("SQL: " + sql);
+						var request = new mssql.Request(connection);
+						request.query(sql, function(err, recordset) {
+							if (!recordset) {
+								loop();
+								return;
+							}
+							for (var row of recordset) {
+								output.append("|")
+								for (var col in row) {
+									output.append(` ${row[col]} |`)
+								}
+								output.appendLine("");
+							}
+							loop();
+						});
+					} else {
+						connection.close();
+					}
+				});
+			}
+		});
 	});
-	
+
 	context.subscriptions.push(disposable);
 }
