67fdee1126ea35d6b8059530f47b6d173b01f2a8
Dick van den Brink <d_vandenbrink@outlook.com>
2015-12-09 20:34:35 +0100

    Removed "use strict" - fixed in latest TypeScript emit We are using @next so a new install was required.

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,5 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-"use strict"
-
 import * as vscode from 'vscode';
 import * as mssql from 'mssql';
 import * as pm from './profileManager';
